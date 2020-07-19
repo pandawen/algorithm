4606034bf3eb047185514b6c576755f9d81d7396
zq <414771342@qq.com>
2020-07-19 11:31:51 +0800

    2020年7月19日11:29:15 项目第一次提交到本地

---
+++ src/com/zq/Main.java
@@ -0,0 +1,16 @@
+package com.zq;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        while(sc.hasNext()){
+//
+//        }
+    }
+
+}
+
+
