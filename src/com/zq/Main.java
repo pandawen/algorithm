9ed93ff3d47c7a55bb9740b21e8ba4f7b00a92af
zq <414771342@qq.com>
2020-07-19 12:37:40 +0800

    2020年7月19日11:29:15 项目第二次提交到本地

--- src/com/zq/Main.java
+++ src/com/zq/Main.java
@@ -5,12 +5,10 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//        while(sc.hasNext()){
-//
-//        }
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            sc.nextLine();
+        }
     }
 
 }
-
-
