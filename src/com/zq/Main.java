b8b466321b42de596de1d0ad82807677a9750a92
zq <414771342@qq.com>
2020-07-21 06:54:49 +0800

    2020年7月21日06:54:21

--- src/com/zq/Main.java
+++ src/com/zq/Main.java
@@ -1,14 +1,21 @@
 package com.zq;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        while(sc.hasNext()){
-            sc.nextLine();
-        }
+//        Scanner sc = new Scanner(System.in);
+//        while(sc.hasNext()){
+//            String s = sc.nextLine();
+//            int num = MyString.cntS(s);
+//            System.out.println(num);
+//        }
+        int[] ints = {11,3,15,2,7,2,9,13,44,2,1,4,8,5,7};
+        MySort.mergeSort(ints);
+        System.out.println(Arrays.toString(ints));;
+        int index = MySort.binarSearch(ints, 7);
+        System.out.println(index);
     }
 
 }
